a99eab8a34342dbd694e3cb59f3304348d5840bd
Aarav Gupta <aarav@spikonado.com>
2025-08-31 10:21:25 +0530

    Replace home page with product page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Chat } from "@/app/product/Chat/Chat";
 import { ChatIntro } from "@/app/product/Chat/ChatIntro";
 import { UserMenu } from "@/components/UserMenu";
 
-export default function ProductPage() {
+export default function HomePage() {
   return (
     <main className="flex max-h-screen grow flex-col overflow-hidden">
       <div className="flex items-start justify-between border-b p-4">
@@ -12,4 +12,4 @@ export default function ProductPage() {
       <Chat />
     </main>
   );
-}
+} 
